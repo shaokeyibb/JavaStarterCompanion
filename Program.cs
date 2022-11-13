27fb236ddac25e35f4f84fdcb1dba33570fb07a8
HikariLan <shaokeyibb@126.com>
2022-11-13 12:45:50 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using JavaStarterCompanion;
+
+if (Settings.ReleaseDefault())
+{
+    Console.WriteLine("Settings has been released to Settings.json, please edit it and restart the program.");
+    Environment.Exit(0);
+}
+
+var settings = Settings.DeserializeJsonFromFile();
+
+if (!File.Exists(settings.JavaPath))
+{
+    Console.WriteLine("Java path is invalid.");
+    Environment.Exit(0);
+}
+
+if (!File.Exists(settings.JarPath))
+{
+    Console.WriteLine("Jar file not found.");
+    Environment.Exit(-1);
+}
+
+var startInfo =
+    new ProcessStartInfo(settings.JavaPath,
+        $"{settings.JavaArgs} -jar {settings.JarPath} {settings.JarArgs}")
+    {
+        CreateNoWindow = true,
+        UseShellExecute = false
+    };
+
+Process.Start(startInfo);
---
+++ Settings.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+
+namespace JavaStarterCompanion;
+
+public class Settings
+{
+    private static string _fileName = "JscSettings.json";
+
+    public Settings(string javaPath, string javaArgs, string jarPath, string jarArgs)
+    {
+        JavaPath = javaPath;
+        JavaArgs = javaArgs;
+        JarPath = jarPath;
+        JarArgs = jarArgs;
+    }
+
+    public string JavaPath { get; }
+    public string JavaArgs { get; }
+    public string JarPath { get; }
+    public string JarArgs { get; }
+
+    private static Settings GenerateDefault()
+    {
+        return new Settings("java", "", "launcher.jar", "");
+    }
+
+    public static bool ReleaseDefault()
+    {
+        if (File.Exists(_fileName)) return false;
+        GenerateDefault().SerializeJsonToFile();
+        return true;
+    }
+
+    public void SerializeJsonToFile()
+    {
+        var json = JsonSerializer.Serialize(this);
+        File.WriteAllText(_fileName, json);
+    }
+
+    public static Settings DeserializeJsonFromFile()
+    {
+        var json = File.ReadAllText(_fileName);
+        return JsonSerializer.Deserialize<Settings>(json) ?? throw new InvalidOperationException();
+    }
+}
